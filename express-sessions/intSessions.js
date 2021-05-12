73d53bd849953d6e91a6daf7b4d5029c2690ffa2
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-11 19:14:33 -0500

    Update intSessions.js

--- express-sessions/intSessions.js
+++ express-sessions/intSessions.js
@@ -1,21 +1,21 @@
 var express = require('express');
 
 var app = express();
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
 var session = require('express-session');
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({secret:'SuperSecretPassword'}));
+app.use(session({ secret: 'SuperSecretPassword' }));
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 6453);
 
-app.get('/',function(req,res,next){
+app.get('/', function (req, res, next) {
   var context = {};
   //If there is no session, go to the main page.
-  if(!req.session.name){
+  if (!req.session.name) {
     res.render('newSession', context);
     return;
   }
@@ -23,31 +23,31 @@ app.get('/',function(req,res,next){
   context.toDoCount = req.session.toDo.length || 0;
   context.toDo = req.session.toDo || [];
   console.log(context.toDo);
-  res.render('toDo',context);
+  res.render('toDo', context);
 });
 
-app.post('/',function(req,res){
+app.post('/', function (req, res) {
   var context = {};
 
-  if(req.body['New List']){
+  if (req.body['New List']) {
     req.session.name = req.body.name;
     req.session.toDo = [];
     req.session.curId = 0;
   }
 
   //If there is no session, go to the main page.
-  if(!req.session.name){
+  if (!req.session.name) {
     res.render('newSession', context);
     return;
   }
 
-  if(req.body['Add Item']){
-    req.session.toDo.push({"name":req.body.name, "id":req.session.curId});
+  if (req.body['Add Item']) {
+    req.session.toDo.push({ "name": req.body.name, "id": req.session.curId });
     req.session.curId++;
   }
 
-  if(req.body['Done']){
-    req.session.toDo = req.session.toDo.filter(function(e){
+  if (req.body['Done']) {
+    req.session.toDo = req.session.toDo.filter(function (e) {
       return e.id != req.body.id;
     })
   }
@@ -56,21 +56,21 @@ app.post('/',function(req,res){
   context.toDoCount = req.session.toDo.length;
   context.toDo = req.session.toDo;
   console.log(context.toDo);
-  res.render('toDo',context);
+  res.render('toDo', context);
 });
 
-app.use(function(req,res){
+app.use(function (req, res) {
   res.status(404);
   res.render('404');
 });
 
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.type('plain/text');
   res.status(500);
   res.render('500');
 });
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function () {
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
